86d9a1e157cb98a0aa1a35016677b9bf6c314ccd
Hua Yanghao <huayanghao@gmail.com>
2012-10-25 18:00:18 +0800

    v0.1 stable

--- example/main.py
+++ example/main.py
@@ -1,3 +1,3 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return "Coding since 2005. %s" % env['REQUEST_URI']
+    return ["Coding since 2005. %s" % env['REQUEST_URI']]
